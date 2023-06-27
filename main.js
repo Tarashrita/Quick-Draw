206abaf29018446f0a41c592ea48839338ef3c36
Tarashrita Ganguly <106442772+Tarashrita@users.noreply.github.com>
2023-06-27 18:37:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var random_number= quick_draw_data_set
+
+console.log("random_number is set to;")
+
+function updateLabel() {
+    document.getElementById("label").innerHTML
+}
+
+var timer_counter= 0;
+var timer_check= timerValue;
+var drawn_sketch=0;
+var answer_holder= matchSketch;
+var score= 0;
