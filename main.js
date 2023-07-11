a47ddd96fd70da4186711a36ff38d095f1a1693e
Tarashrita Ganguly <106442772+Tarashrita@users.noreply.github.com>
2023-07-11 18:16:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var random_number= quick_draw_data_set
-
 console.log("random_number is set to;")
+
+var random_number = Math.random();
 
 function updateLabel() {
     document.getElementById("label").innerHTML
@@ -9,5 +9,31 @@ function updateLabel() {
 var timer_counter= 0;
 var timer_check= timerValue;
 var drawn_sketch=0;
-var answer_holder= matchSketch;
+var answer_holder= checkSketch();
 var score= 0;
+
+
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center(); 
+    background("white");
+}
+
+function clearCanvas() {
+    background("white");
+}
+
+function checkSketch() {
+    
+    if(timer_counter>400) 
+    {
+      timer_counter = counter++;
+      timer_check = "completed";
+    }
+
+    if(timer_check=completed) 
+    {
+        timer_check=0
+        updateCanvas()
+    }
+}
